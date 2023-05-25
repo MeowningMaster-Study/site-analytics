2bc4301d88882d9ea75c057c3216452be5a9d17e
MeowningMaster <meowningmaster@gmail.com>
2023-05-25 13:58:04 +0300

    allow cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,11 @@ import fastify from 'fastify'
 import { eventController } from './components/event/controller.js'
 import { logger } from './logger.js'
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
+import cors from '@fastify/cors'
 
 const server = fastify.default({}).withTypeProvider<TypeBoxTypeProvider>()
 
+server.register(cors.default)
 server.register(eventController, { prefix: '/event' })
 
 const port = 8080
